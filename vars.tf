fe86a168f7b49794dee46fdde32b4f86f6c819c5
kalis <kalis30nov@yahoo.com>
2023-05-28 12:14:14 +0530

    update

--- vars.tf
+++ vars.tf
@@ -2,9 +2,9 @@ variable "instance_type" {}
 variable "subnet_ids" {}
 variable "name" {}
 variable "env" {}
-variable "vpc_id"
+variable "vpc_id" {}
 variable "allow_app_cidr" {}
 variable "bastion_cidr" {}
-variable "desired_capacity"
-variable "max_size"
-variable "min_size"
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
